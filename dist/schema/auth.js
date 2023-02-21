67e35f549f18f0ed01f3065a306ed3aae41fc224
d0tf <naufalsuryalaga24@gmail.com>
2023-02-21 13:16:25 +0700

    remove: build folder

--- dist/schema/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const zod_1 = require("zod");
-exports.default = zod_1.z.object({
-    username: zod_1.z.string(),
-    password: zod_1.z.string().min(8),
-});
-//# sourceMappingURL=auth.js.map
